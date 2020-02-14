918f4a1a648d2cabe0564750c1c574bc832786f8
huynn <huy.nguyen3@beesightsoft.com>
2020-02-14 17:58:32 +0700

    Init and test data

---
+++ src/KeToanDTO/SoLieuTonKhoDto.cs
@@ -0,0 +1,14 @@
+﻿using EPPlus.Core.Extensions.Attributes;
+
+namespace KeToanDTO
+{
+    public class SoLieuTonKhoDto
+    {
+        [ExcelTableColumn("TT")]
+        public int Stt { get; set; }
+
+        [ExcelTableColumn(@"Mã Hàng")]
+        public string MaHang { get; set; }
+        //public int TenHang { get; set; }
+    }
+}
---
+++ src/ToolKeToanConvertWindowsForms/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using EPPlus.Core.Extensions;
+using KeToanDTO;
+using OfficeOpenXml;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ToolKeToanConvertWindowsForms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            ReadDataTonDau();
+        }
+
+        private void ReadDataTonDau()
+        {
+            string path = "D://so lieu ton kho.xlsx";
+            FileInfo fileInfo = new FileInfo(path);
+
+            ExcelPackage package = new ExcelPackage(fileInfo);
+            // Direct usage: 
+            package.ToList<SoLieuTonKhoDto>(1, configuration => configuration.SkipCastingErrors());
+            // Specific worksheet: 
+            var soLieuTonDau = package.GetWorksheet(1).ToList<SoLieuTonKhoDto>();
+        }
+    }
+}
